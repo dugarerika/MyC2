88bc5b1792c8b94d5e3e07af584fbea5a8a33a3f
Erika Maria Tavera Misas <etavera-@lab2r4s10.42abudhabi.ae>
2023-02-08 11:27:21 +0400

    printf bases

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,35 @@
 /*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 09:12:39 by etavera-          #+#    #+#             */
-/*   Updated: 2023/02/08 08:26:57 by etavera-         ###   ########.fr       */
+/*   Updated: 2023/02/08 11:26:08 by etavera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "printf.h"
 
-int ft_printf(const char *, ...)
+int	ft_printf(const char *fstr, ...)
 {
+	va_list	ptr;
+	int		i;
+	int		j;
+
+	va_start(ptr, fstr);
+	i = 0;
+	j = 0;
+
+	while (fstr)
+	{
+		if (fstr[i] = '%')
+		{
+
+		}
+	}
+
+	va_end(ptr);
+}
+
+int main()
+{
+
 
 }
--- printf.h
+++ printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 10:03:09 by etavera-          #+#    #+#             */
-/*   Updated: 2023/02/08 10:06:26 by etavera-         ###   ########.fr       */
+/*   Updated: 2023/02/08 10:44:11 by etavera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
 # include <stdarg.h>
 
-int ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
+#endif
