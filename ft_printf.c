c86e0f01770d7e30174ef88dd616a91168e76594
Erika Tavera <dugarerika@gmail.com>
2023-02-24 15:52:17 +0400

    fixing hexa

--- ft_printf.c
+++ ft_printf.c
@@ -15,23 +15,6 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-int	ft_power(int nb, int power)
-{
-	int	k;
-	int	acum;
-
-	k = 0;
-	acum = 1;
-	if (power < 0)
-		return (0);
-	while (k < power)
-	{
-		acum = acum * nb;
-		k++;
-	}
-	return (acum);
-}
-
 void	ft_putstr(char *str)
 {
 	int	i;
@@ -46,61 +29,7 @@ void	ft_putstr(char *str)
 	}
 }
 
-void	put_oct(int d)
-{
-	int	p;
-	int	decimal;
-	int	r;
 
-	p = 0;
-	decimal = 0;
-
-	while (n > 0)
-	{
-		r = n % 10;
-		decimal = decimal + r * ft_power(8, p);
-		p++;
-	}
-	ft_putstr(decimal);
-}
-
-void	put_hex(int d, char base)
-{
-	char	mod;
-	char	*acum;
-	int	i;
-	int	r;
-
-	i = -1;
-	r = d;
-	while (r != 0)
-	{
-		r = r / 16;
-		i++;
-	}
-	acum = malloc(i);
-	while (d != 0)
-	{
-		mod = d % 16;
-		if (mod <= 15 && mod >= 10)
-		{
-			if (base == 'X')
-				mod = mod + 55;
-			else if (base == 'x')
-				mod = mod + 87;
-			acum[i] = mod;
-			i--;
-		}
-		else if (mod <= 9)
-		{
-			mod = mod + 48;
-			acum[i] = mod;
-			i--;
-		}
-		d = d / 16;
-	}
-	ft_putstr(acum);
-}
 
 void	ft_putnbr(int a)
 {
@@ -130,7 +59,66 @@ void	ft_putnbr(int a)
 	}
 }
 
-void	ft_str_is_numeric(char str)
+void	put_hex(int d, char base)
+{
+	char	mod;
+	char	*acum;
+	int	i;
+	int	r;
+
+	i = -1;
+	r = d;
+	while (r != 0)
+	{
+		r = r / 16;
+		i++;
+	}
+	acum = malloc(i);
+	if(d == 0)
+	ft_putnbr(0);
+	while (d != 0)
+	{
+		
+		mod = d % 16;
+		if (mod <= 15 && mod >= 10)
+		{
+			if (base == 'X')
+				mod = mod + 55;
+			else if (base == 'x')
+				mod = mod + 87;
+			acum[i] = mod;
+			i--;
+		}
+		else if (mod <= 9)
+		{
+			mod = mod + 48;
+			acum[i] = mod;
+			i--;
+		}
+		d = d / 16;
+	}
+	ft_putstr(acum);
+	free(acum);
+}
+
+void	put_oct(int d)
+{
+	int	i;
+	int	octal;
+
+	i = 1;
+	octal = 0;
+
+	while (d)
+	{
+		octal += (d % 8) * i;
+		d /= 8;
+		i *=10;
+	}
+	ft_putnbr(octal);
+}
+
+void	ft_str_is_character(char str)
 {
 	int	result;
 
@@ -145,44 +133,43 @@ void	ft_str_is_numeric(char str)
 		}
 }
 
-void	ft_atoi(char *str)
-{
-	int	neg;
-	int	result;
-	int	k;
+// void	ft_atoi(char *str)
+// {
+// 	int	neg;
+// 	int	result;
+// 	int	k;
 
-	k = 0;
-	neg = 1;
-	result = 0;
-	while (str[k] == 32 || (str[k] >= 9 && str[k] <= 13))
-	{
-		k++;
-	}
-	if (str[k] == 45 || str[k] == 43)
-	{
-		if (str[k] == 45)
-		{
-			neg = neg * -1;
-		}
-	k++;
-	}
-	while (str[k] <= 57 && str[k] >= 48)
-	{
-		result = (result * 10) + (str[k] - 48);
-		k++;
-	}
-	result = result * neg;
-	ft_putnbr(result);
+// 	k = 0;
+// 	neg = 1;
+// 	result = 0;
+// 	while (str[k] == 32 || (str[k] >= 9 && str[k] <= 13))
+// 	{
+// 		k++;
+// 	}
+// 	if (str[k] == 45 || str[k] == 43)
+// 	{
+// 		if (str[k] == 45)
+// 		{
+// 			neg = neg * -1;
+// 		}
+// 	k++;
+// 	}
+// 	while (str[k] <= 57 && str[k] >= 48)
+// 	{
+// 		result = (result * 10) + (str[k] - 48);
+// 		k++;
+// 	}
+// 	result = result * neg;
+// 	ft_putnbr(result);
 
-}
+// }
 
 void	ft_check_specifier(const char spcr, va_list ptr)
 {
-
 	if (spcr == 'i' || spcr == 'd')
 		ft_putnbr(va_arg(ptr, int));
 	if (spcr == 'c')
-		ft_str_is_numeric((char)va_arg(ptr, int));
+		ft_str_is_character((char)va_arg(ptr, int));
 	if (spcr == 's')
 		ft_putstr(va_arg(ptr, char *));
 	if (spcr == 'p')
@@ -190,7 +177,7 @@ void	ft_check_specifier(const char spcr, va_list ptr)
 	if (spcr == 'f')
 		write(1, "floating", 8);
 	if (spcr == 'o')
-		write(1, "octal number", 12);
+		put_oct(va_arg(ptr, int));
 	if (spcr == 'u')
 		ft_putnbr(va_arg(ptr, int));
 	if (spcr == 'x' || spcr == 'X')
@@ -228,33 +215,54 @@ int ft_printf(const char *fstr, ...)
 
 }
 
-int	main(void)
-{
-	char str[] = " -+++++-01234ab567";
-	// printf ("Integers: %u %i \n", -3456, 3456);
-	// ft_printf ("Integers: %u %i \n", -3456, 3456);
-	// printf ("Characters: %c %c \n", "z", 80);
-    // ft_printf ("Characters: %c %c \n", "z", 80) //need to check this scenario;
-	ft_printf ("Characters: %c \n", 'z');
-	ft_printf ("Characters: %c \n", 80);
-	// printf ("Decimals: %d %ld\n", 1997, 32000L);
-	// printf ("floats: %4.2f %+.0e %E \n", 3.14159, 3.14159, 3.14159);
-	// printf ("Preceding with empty spaces: %10d \n", 1997);
-	// printf ("Preceding with zeros: %010d \n", 1997);
-	ft_printf ("Preceding with zeros: %i \n", 1997);
-	ft_printf ("Preceding with zeros: %d \n", 1997);
-	ft_printf ("Preceding with zeros: %s %d \n", "abc", 12);
-	ft_printf ("%d %d \n", 1997, 12);
-	// printf ("Width: %*d \n", 15, 140);
-	// ft_printf ("Width: %*d \n", 15, 140);
-	ft_printf("%%|\n");
-	printf("%%|\n");
-	ft_printf("%#8x|\n", 234567);
-	printf("%#8x|\n", 234567);
-	ft_printf ("%s \n", "Educative");
-	printf("%x %x %x %x %x %x %x %x\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
-	ft_printf("%x %x %x %x %x %x %x %x\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
-	printf("%X %X %X %X %X %X %X %X\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
-	ft_printf("%X %X %X %X %X %X %X %X\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
-	printf("%d", atoi(str));
-}
+// int	main(void)
+// {
+// 	char str[] = " -+++++-01234ab567";
+// 	// printf ("Integers: %u %i \n", -3456, 3456);
+// 	// ft_printf ("Integers: %u %i \n", -3456, 3456);
+// 	// printf ("Characters: %c %c \n", "z", 80);
+//     // ft_printf ("Characters: %c %c \n", "z", 80) //need to check this scenario;
+// 	printf ("Octal value is : %o \n", 2567);
+// 	ft_printf ("Octal value is : %o \n", 2567);
+// 	ft_printf ("Characters: %c \n", 'z');
+// 	ft_printf ("Characters: %c \n", 80);
+// 	// printf ("Decimals: %d %ld\n", 1997, 32000L);
+// 	// printf ("floats: %4.2f %+.0e %E \n", 3.14159, 3.14159, 3.14159);
+// 	// printf ("Preceding with empty spaces: %10d \n", 1997);
+// 	// printf ("Preceding with zeros: %010d \n", 1997);
+// 	printf ("Preceding with zeros: %i \n", 1997);
+// 	ft_printf ("Preceding with zeros: %i \n", 1997);
+// 	ft_printf ("Preceding with zeros: %d \n", 1997);
+// 	ft_printf ("Preceding with zeros: %s %d \n", "abc", 12);
+// 	ft_printf ("%d %d \n", 1997, 12);
+// 	// printf ("Width: %*d \n", 15, 140);
+// 	// ft_printf ("Width: %*d \n", 15, 140);
+// 	printf("%i\n", 0);
+// 	ft_printf("%i\n", 0);
+// 	printf("%i\n", -1);
+// 	ft_printf("%i\n", -1);
+// 	printf("%i\n", 0);
+// 	ft_printf("%x\n", 0);
+// 	printf("%x\n", 100);
+// 	ft_printf("%x\n", 100);
+// 	printf("%x\n", -1);
+// 	ft_printf("%x\n", -1);
+// 	printf("%x\n", 9);
+// 	ft_printf("%x\n", 9);
+// 	printf("%x\n", 2147483647);
+// 	ft_printf("%x\n", 2147483647);
+// 	printf("%x\n", 32768);
+// 	ft_printf("%x\n", 32768);
+
+// 	//printf("%x %x %x %x %x %x %x", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+// 	ft_printf("%%|\n");
+// 	printf("%%|\n");
+// 	ft_printf("%#8x|\n", 234567);
+// 	printf("%#8x|\n", 234567);
+// 	ft_printf ("%s \n", "Educative");
+// 	printf("%x %x %x %x %x %x %x %x\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
+// 	ft_printf("%x %x %x %x %x %x %x %x\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
+// 	printf("%X %X %X %X %X %X %X %X\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
+// 	ft_printf("%X %X %X %X %X %X %X %X\n", 0xC0, 0xC0, 0x61, 0x62, 0x63, 0x31, 0x32, 0x33);
+// 	printf("%d", atoi(str));
+// }
