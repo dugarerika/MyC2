ed669e6cc09a084ca29174f81f5984821171bee5
Erika Tavera <dugarerika@gmail.com>
2023-03-09 06:32:44 +0400

    fixes

--- putstr.c
+++ putstr.c
@@ -3,13 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   putstr.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: erikadugar <erikadugar@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:58:13 by erikadugar        #+#    #+#             */
-/*   Updated: 2023/03/07 11:46:30 by etavera-         ###   ########.fr       */
+/*   Updated: 2023/03/09 06:24:16 by erikadugar       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-6tgrfd
+
 #include "ft_printf.h"
 
 int	ft_strlen(const char *str)
